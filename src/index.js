c07c775907adc07d47743b4a285a25d06d1ae7b1
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2023-12-31 14:37:25 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const {fetch} = require("undici");
+
+const {WEBHOOK_URL} = process.env;
+
+async function getWords() {
+
+    const wordsURL = "https://raw.githubusercontent.com/lrojas94/SAT-Words/master/Freevocabulary%20Wordlist/freevocabulary_words.json";
+
+    const res = await fetch(wordsURL);
+    const json = await res.json();
+
+    return json;
+}
+
+async function sendWord(wordData) {
+
+    const {word, definition} = wordData;
+
+    const embed = { 
+        color: 16562691,
+        fields: [
+            {
+                name: "Word", 
+                value: `\`${word}\``, 
+            },  
+            {
+                name: "Definiton", 
+                value: `\`${definition}\``
+            }
+        ]
+    }
+
+    await fetch(WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            embeds: [embed]
+        }),
+    })
+
+}
+
+async function sendRandomWord(wordList) {
+    const selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+    await sendWord(selectedWord);
+}
+
+(async () => {
+
+    const wordList = await getWords();
+
+    await sendRandomWord(wordList);
+
+    setInterval(async () => {
+
+        const now = new Date();
+
+        const hr = now.getHours();
+        const min = now.getMinutes();
+
+        if (min == 0 && hr % 2 == 0 && hr > 7 && hr < 23) {
+            await sendRandomWord(wordList);
+        }
+
+    }, 60000)
+
+})()
+
+console.log("Words started!");
